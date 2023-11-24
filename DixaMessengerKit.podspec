89e49f47b98e08f7568ac02f501b0f90484b3628
DixaRobot <githubbot@dixa.com>
2023-11-24 14:06:56 +0000

    Release 1.6.8

--- DixaMessengerKit.podspec
+++ DixaMessengerKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'DixaMessengerKit'
-  s.version          = '1.6.7'
+  s.version          = '1.6.8'
   s.summary          = 'DixaMessenger for iOS'
   
 # This description is used to generate tags and improve search results.
--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
             targets: ["DixaMessenger"]),
     ],
     targets: [
-        .binaryTarget(name: "DixaMessenger", url: "https://github.com/dixahq/ios-messenger/releases/download/1.6.7/DixaMessenger.xcframework.zip", checksum: "9e41266a8f600fa9c4a2c7c76f37f4b2e70c85e92e60cec317b2dd97bd61c0d3")
+        .binaryTarget(name: "DixaMessenger", url: "https://github.com/dixahq/ios-messenger/releases/download/1.6.8/DixaMessenger.xcframework.zip", checksum: "2a3814abb1bbb43e6f0afa39663a0ed3a4befc35b101836fe314cdca818c7299")
     ]
 )
