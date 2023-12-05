a9fad2505888153a8b4cd271b2b5cc4083577670
DixaRobot <githubbot@dixa.com>
2023-12-05 12:29:20 +0000

    Release 1.6.9

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
             targets: ["DixaMessenger"]),
     ],
     targets: [
-        .binaryTarget(name: "DixaMessenger", url: "https://github.com/dixahq/ios-messenger/releases/download/1.6.9/DixaMessenger.xcframework.zip", checksum: "4a1a8aa255d98f68f2bae0f35fe7fc7bfdf1c87174378997e02b071e01ae5069")
+        .binaryTarget(name: "DixaMessenger", url: "https://github.com/dixahq/ios-messenger/releases/download/1.6.9/DixaMessenger.xcframework.zip", checksum: "9b800cdf2220aef45dac38d87911b39e5a1271e6dbd0bd002802decfbf470be2")
     ]
 )
